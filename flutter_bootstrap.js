c93bd3ae53e280b575f3a75be62d3944f664623b
Shahbaz Khan <shahbazvidicraze@gmail.com>
2025-05-20 08:57:36 +0500

    5/20/25

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1469319430"
+    serviceWorkerVersion: "2625823580"
   }
 });
