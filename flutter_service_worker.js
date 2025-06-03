2bf79e25fb1be63b143085be6cb87a1b78ccb2b6
Shahbaz Khan <shahbazvidicraze@gmail.com>
2025-06-04 04:48:30 +0500

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "63d3db38ae486b2ffe2907437886a806
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2c04bdb6642b6f4305f88dc3f85ed39f",
+"flutter_bootstrap.js": "a6dc551f4afd70de18c74f4e59624131",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
